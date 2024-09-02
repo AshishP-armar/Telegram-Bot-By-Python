5cf8a945fa89911de9fbd59da5c50a6c80352539
AshishP-armar <atp4466@gmail.com>
2024-09-02 20:33:24 +0530

    message send add

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,3 +1,10 @@
-import requests
+import requests #pip install requests
+
 token = "api_key"
 chat_id = "chat_id" # use the chat id with - this 
+
+# *************** To Send Message **********************
+def send_text(message):
+	base_url= f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
+	requests.get(base_url)
+	
