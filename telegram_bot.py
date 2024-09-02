cc08bbc5b3514945f8c7f8dfd201034176d4b23f
AshishP-armar <atp4466@gmail.com>
2024-09-02 20:37:05 +0530

    photo sending add

--- telegram_bot.py
+++ telegram_bot.py
@@ -14,3 +14,9 @@ def send_link(link):
 	link_message = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={link}"
 	requests.get(link_message)
 	
+
+# ****************** Photo Sending ****************
+def send_photo_caption(file,caption):
+    files_photo = {'photo':open('file path','rb')}
+    responce_photo= requests.post(f"https://api.telegram.org/bot{token}/sendPhoto?chat_id=-4563384288&caption=Test Caption Image By Bot😡😭😞😚😍",files=files_photo)
+    print(responce_photo)
